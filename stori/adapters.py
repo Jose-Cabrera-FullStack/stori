06d30c86512b6902cfc18ec064f446a679b3b263
Jose Cabrera <murdoc.jose.6@gmail.com>
2023-08-23 17:03:55 -0300

    feat(models): added transaction model

--- stori/adapters.py
+++ stori/adapters.py
@@ -1,8 +1,22 @@
 # Connect the service with external services from infrastructure. (S3, Other APIs, etc.)
+import csv
+
+# Extract data from csv file
+# Path: stori/temp/stori_transaction_data.csv
+# Format:
+# "Id,Date,Transaction"
+# "0,7/15,+60.5"
+# "1,7/28,-10.3"
+# "2,8/2,-20.46"
+# "3,8/13,+10"
+
 
 class StoriAdapter:
 
     @staticmethod
-    def get_token(credentials: str) -> str:
-        """ Get token from external service. """
-        return
+    def extract_data_from_csv(file_path: str) -> list:
+        """ Extract data from csv file. """
+
+        with open(file_path, 'r') as csv_file:
+            reader = csv.DictReader(csv_file)
+            return [row for row in reader]
--- stori/models.py
+++ stori/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Transaction(models.Model):
+    TRANSACTION_TYPES = (
+        ('credit', 'Credit'),
+        ('debit', 'Debit'),
+    )
+
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+    transaction_type = models.CharField(
+        max_length=10,
+        choices=TRANSACTION_TYPES
+    )
+
+    def __str__(self):
+        return f"{self.transaction_type} - {self.amount} ({self.date})"
